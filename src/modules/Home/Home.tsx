d8a0dc80fb39299b50bd15011cf306f41482a619
the43rd <ismailmuyideen.c05@gmail.com>
2024-06-23 06:05:21 +0100

    revamped the homepage

--- src/modules/Home/Home.tsx
+++ src/modules/Home/Home.tsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import styles from "./home.module.css"
 import Footer from "src/shared/components/Footer/Footer";
 import Header from "src/shared/components/Header/Header";
@@ -10,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <Sidebar />
-        <div></div>
+        <Outlet />
       </main>
 
       <Footer />
