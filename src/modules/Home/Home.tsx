c8b2664c848c3a56e76fabf64437a2cd9f279b34
the43rd <ismailmuyideen.c05@gmail.com>
2024-06-26 04:32:12 +0100

    adjusted width of page

--- src/modules/Home/Home.tsx
+++ src/modules/Home/Home.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import styles from "./home.module.css"
+import styles from "./home.module.css";
 import Footer from "src/shared/components/Footer/Footer";
 import Header from "src/shared/components/Header/Header";
 import Sidebar from "src/shared/components/Sidebar/Sidebar";
