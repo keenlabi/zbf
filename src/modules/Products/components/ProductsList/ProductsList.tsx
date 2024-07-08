4fb643c1a2cbd8018277d89911191bbc8480ee93
Keenlabi <keenlabinc@gmail.com>
2024-07-08 02:20:20 +0100

    show no products in category message

--- src/modules/Products/components/ProductsList/ProductsList.tsx
+++ src/modules/Products/components/ProductsList/ProductsList.tsx
@@ -1,35 +1,40 @@
 import styles from "./productslist.module.css";
 import { useEffect, useState } from "react";
 import { FetchProductsByCategoryAction } from "src/shared/api/products.api";
-import { INetworkResponse } from "src/shared/api/types";
 import DataLoadingError from "src/shared/components/DataLoadingError";
 import GridList from "src/shared/components/List/GridList";
 import CircularRingLoader from "src/shared/components/Loaders/CircularRingLoader";
 import Pagination from "src/shared/components/Pagination";
 import ProductCard from "src/shared/components/ProductCard";
 import { useProductCategoriesStateValue } from "src/store/products/productCategories/productCategories.atom";
-import { IProductListItem, useProductsState } from "src/store/products/products.atom";
+import { useProductsState } from "src/store/products/products.atom";
 
 export default function ProductsList() {
 
   const productCategoriesState = useProductCategoriesStateValue();
-  const [productState, setProductState] = useProductsState();
+  const [productsState, setProductsState] = useProductsState();
 
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
-  const [networkResponse, setNetworkResponse] = useState<INetworkResponse<{products:IProductListItem[]}>>();
+  // const [networkResponse, setNetworkResponse] = useState<INetworkResponse<{products:IProductListItem[]}>>();
 
   useEffect(()=> {
     setIsLoading(true);
-    FetchProductsByCategoryAction(productCategoriesState.activeCategory, productState.products.currentPage)
-    .then((response)=> setNetworkResponse(response))
+    FetchProductsByCategoryAction(productCategoriesState.activeCategory, productsState.products.currentPage)
+    .then((response)=> setProductsState(state => ({
+      ...state,
+      products: {
+        ...state.products,
+        list: response.data.products
+      }
+    })))
     .catch((error)=> setErrorMessage(error.message))
     .finally(()=> setIsLoading(false))
 
-  }, [productCategoriesState.activeCategory, productState.products.currentPage])
+  }, [productCategoriesState.activeCategory, productsState.products.currentPage, setProductsState])
 
   function paginate(newPage:number) {
-    setProductState(state=> ({
+    setProductsState(state=> ({
       ...state,
       products:{
         ...state.products,
@@ -46,11 +51,11 @@ export default function ProductsList() {
       {
         isLoading
         ? <CircularRingLoader color={"red"} />
-        : networkResponse?.statusCode === 200 &&
-            <GridList columnCount={4}>
+        : productsState.products.list.length
+          ? <GridList columnCount={4}>
               {
-                networkResponse.data.products?.map((product) => {
-                  return  <ProductCard 
+                productsState.products.list?.map((product) => {
+                  return  <ProductCard
                             key={product.id} 
                             id={product.id} 
                             name={product.name} 
@@ -61,11 +66,12 @@ export default function ProductsList() {
                 })
               }
             </GridList>
+          : <div> No products found for this category </div>
       }
 
       <Pagination
-        currentPage={productState.products.currentPage}
-        totalPages={productState.products.totalPages}
+        currentPage={productsState.products.currentPage}
+        totalPages={productsState.products.totalPages}
         goToPageAction={(newPage:number)=> paginate(newPage)}
       />
     </div> 
