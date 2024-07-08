b5bf05776770ebe83303f78ead958ce1e2637c76
Keenlabi <keenlabinc@gmail.com>
2024-07-08 02:31:07 +0100

    added some effects to product card

--- src/shared/components/ProductCard/ProductCard.tsx
+++ src/shared/components/ProductCard/ProductCard.tsx
@@ -1,9 +1,10 @@
 import { useNavigate } from "react-router-dom";
-import IconButton from "../Buttons/IconButton/IconButton";
+// import IconButton from "../Buttons/IconButton/IconButton";
 import styles from "./productcard.module.css";
-import IconCart from "src/shared/assets/icons/cart.svg";
+// import IconCart from "src/shared/assets/icons/cart.svg";
 import Image from "src/shared/assets/images/shoes.png";
-import { useAuthStateValue } from "src/store/auth/auth.atom";
+import formatCurrency from "src/shared/utility/formatCurrency";
+// import { useAuthStateValue } from "src/store/auth/auth.atom";
 
 interface ProductCardProps {
   id:string;
@@ -15,7 +16,7 @@ interface ProductCardProps {
 
 export default function ProductCard(props:ProductCardProps) {
   const navigate = useNavigate();
-  const authState = useAuthStateValue();
+  // const authState = useAuthStateValue();
 
   return (
     <div className={styles.ProductContainer} onClick={() => navigate(props.id)}>
@@ -26,18 +27,17 @@ export default function ProductCard(props:ProductCardProps) {
         <div className={styles.productName}>{props.name}</div>
 
         <div className={styles.productPrice}>
-          <span className={styles.dollarSign}>$</span>
-          {props.price.toString().split(".")[0]}
+          {formatCurrency({amount: props.price})}
         </div>
       </div>
-      {
+      {/* {
         authState.isAuthenticated &&
         <IconButton 
           extraStyle={styles.productCardButton} 
           prefixIcon={<img src={IconCart} alt="" />} 
           label="Add to Cart" action={() => {}} 
         />
-      }
+      } */}
     </div>
   );
 }
