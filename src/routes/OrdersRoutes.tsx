da38c3870eca2c7b927ba4b43d04e14295bc3cb1
Keenlabi <keenlabinc@gmail.com>
2024-07-22 08:08:57 +0100

    reverted the last commit

--- src/routes/OrdersRoutes.tsx
+++ src/routes/OrdersRoutes.tsx
@@ -1,11 +1,10 @@
 import Orders from "src/modules/Orders/Orders";
 import { RouteObject } from "react-router-dom";
-import ProtectedRoute from "./ProtectedRoute";
 
 const OrderRoutes:RouteObject[] = [
     {
         path: "/orders",
-        element: <ProtectedRoute children={<Orders /> } authRequired={true} allowedRoles={["customer"]} />
+        element: <Orders />
     },
 ];
 
--- src/shared/components/Header/Header.tsx
+++ src/shared/components/Header/Header.tsx
@@ -42,7 +42,6 @@ export default function Header() {
                 </li>
                 {
                   authState.isAuthenticated &&
-                  userState.profile.role === "customer" &&
                   <li>
                     <Link to="/orders">Orders</Link>
                   </li>
